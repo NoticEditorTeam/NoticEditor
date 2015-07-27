7c19765c62f3527d15b296af786116295f933d37
SeTSeR <sergey200018@bk.ru>
2015-07-27 16:15:13 +0300

    First prototype

---
+++ src/com/temporaryteam/noticeditor/Main.java
@@ -0,0 +1,52 @@
+package com.temporaryteam.noticeditor;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	private Stage primaryStage;
+	private BorderPane rootLayout;
+
+	@Override
+	public void start(Stage primaryStage) {
+		this.primaryStage = primaryStage;
+		this.primaryStage.setTitle("NoticEditor");
+		initRootLayout();
+	}
+
+	
+	/**
+	 * Initializes root layout
+	 */
+	public void initRootLayout() {
+		try {
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(Main.class.getResource("view/Main.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Returns primary stage
+	 */
+	public Stage getPrimaryStage() {
+		return primaryStage;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
