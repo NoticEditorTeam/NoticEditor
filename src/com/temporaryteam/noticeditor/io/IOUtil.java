283fa3a9f546f4c85e38d992dd9f45a6e056af38
Victor <melnikukr@mail.ru>
2015-08-03 16:57:58 +0300

    Move IO operations to separate class IOUtil

---
+++ src/com/temporaryteam/noticeditor/io/IOUtil.java
@@ -0,0 +1,75 @@
+package com.temporaryteam.noticeditor.io;
+
+import java.io.*;
+import java.net.URI;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public final class IOUtil {
+	
+	private static final String NEW_LINE = System.lineSeparator();
+
+	public static String readContent(File file) throws IOException {
+		final StringBuilder result = new StringBuilder();
+		try (InputStream is = new FileInputStream(file);
+				Reader isr = new InputStreamReader(is, "UTF-8");
+				BufferedReader reader = new BufferedReader(isr)) {
+			result.append(reader.readLine()).append(NEW_LINE);
+		}
+		return result.toString();
+	}
+	
+	public static void writeContent(File file, String content) throws IOException {
+		try (OutputStream os = new FileOutputStream(file);
+				Writer writer = new OutputStreamWriter(os, "UTF-8")) {
+			writer.write(content);
+		}
+	}
+	
+	public static void writeJson(File file, JSONObject json) throws IOException, JSONException {
+		try (OutputStream os = new FileOutputStream(file);
+				Writer writer = new OutputStreamWriter(os, "UTF-8")) {
+			json.write(writer);
+		}
+	}
+	
+	/** 
+	 * Pack directory
+	 */
+	public static void pack(File directory, String toSave) throws IOException {
+		final URI root = directory.toURI();
+		Deque<File> queue = new LinkedList<File>();
+		queue.push(directory);
+		
+		try (OutputStream out = new FileOutputStream(new File(toSave));
+				ZipOutputStream zout = new ZipOutputStream(out)) {
+			
+			while(!queue.isEmpty()) {
+				directory = queue.pop();
+				for(File child : directory.listFiles()) {
+					String name = root.relativize(child.toURI()).getPath();
+					if (child.isDirectory()) {
+						queue.push(child);
+						name = name.endsWith("/") ? name : (name + "/");
+						zout.putNextEntry(new ZipEntry(name));
+					} else {
+						zout.putNextEntry(new ZipEntry(name));
+						try (InputStream in = new FileInputStream(child)) {
+							byte[] buffer = new byte[1024];
+							while(true) {
+								int readCount = in.read(buffer);
+								if (readCount < 0) break;
+								zout.write(buffer, 0, readCount);
+							}
+						}
+						zout.closeEntry();
+					}
+				}
+			}
+		}
+	}
+}
